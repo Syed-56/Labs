7b3f19b411a638f2c217541221739b1456aadf96
Syed-56 <ssa.56.sultan@gmail.com>
2025-09-28 18:00:05 +0500

    update

---
+++ DSA/Lab05/task5.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+#define N 9   
+
+bool isSafe(int board[N][N], int row, int col, int num) {
+    for (int x = 0; x < N; x++) {
+        if (board[row][x] == num || board[x][col] == num)
+            return false;
+    }
+
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[startRow + i][startCol + j] == num)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool sudokoSolver(int board[N][N]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            if (board[row][col] == 0) { 
+                for (int num = 1; num <= 9; num++) {
+                    if (isSafe(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (sudokoSolver(board))
+                            return true; 
+                        board[row][col] = 0; 
+                    }
+                }
+                return false; 
+            }
+        }
+    }
+    return true; 
+}
+
+int main() {
+    int board[9][9] = {
+        {5,3,0,0,7,0,0,0,0},
+        {6,0,0,1,9,5,0,0,0},
+        {0,9,8,0,0,0,0,6,0},
+        {8,0,0,0,6,0,0,0,3},
+        {4,0,0,8,0,3,0,0,1},
+        {7,0,0,0,2,0,0,0,6},
+        {0,6,0,0,0,0,2,8,0},
+        {0,0,0,4,1,9,0,0,5},
+        {0,0,0,0,8,0,0,7,9}
+    };
+
+    if (sudokoSolver(board)) {
+        cout << "Sudoku Solved:\n";
+    } else {
+        cout << "No solution exists.\n";
+    }
+}
