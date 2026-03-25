b6ece70a500707766900e1880a8a0fb325f6f47a
Syed-56 <ssa.56.sultan@gmail.com>
2026-03-25 21:29:17 +0500

    update

--- OS/os-lab3/concepts/8_path_manipulating.sh
+++ OS/os-lab3/concepts/8_path_manipulating.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-FILE="~/Documents/Labs/OS/mid-syllabus.txt"
+FILE="$HOME/Documents/Labs/OS/mid-syllabus.txt"
 fileName=$(basename "$FILE")
 dirName=$(dirname "$FILE")
 fullPath=$(realpath "$FILE")
 
-echo "Full String: $File"
+echo "Full String: $FILE"
 echo "File Name: $fileName"
 echo "Directory Name: $dirName"
 echo "Full Path: $fullPath"
@@ -17,7 +17,7 @@ echo "Full Path: $fullPath"
 #example: ./backup_script.sh data.txt
 
 #check if file or folder exist
-folder="../os-lab3"
+folder="../../os-lab3"
 if [ -d $folder ]
 then
     echo "$folder exists"
@@ -26,7 +26,7 @@ else
 fi
 
 filePath="../../mid-syllabus.txt"
-if [! -f filePath ]
+if [ ! -f filePath ]
 then 
     echo "$filePath exists"
 else 
--- OS/os-lab3/concepts/9_whileLoop-usecases.sh
+++ OS/os-lab3/concepts/9_whileLoop-usecases.sh
@@ -4,7 +4,7 @@
 while read -r var   #use -r to avoid escaping backslash
 do
     echo $var
-done < basics.sh
+done < 1_basics.sh
 
 #read content from a csv file
 while IFS="," read -r f1 f2 f3
@@ -15,7 +15,7 @@ do
 done <data.csv
 
 #another way
-cat data.csv | awk awk 'NR>1' | 
+cat data.csv | awk 'NR>1' | 
 while IFS="," read -r f1 f2 f3
 do
     echo "$f1, $f2, $f3"
