94a6ecae45c40bd40c05dbfd561c9070c5463193
Syed-56 <ssa.56.sultan@gmail.com>
2026-03-30 00:18:18 +0500

    update

--- OS/Qs/temp.c
+++ OS/Qs/temp.c
@@ -5,5 +5,9 @@
 #include <fcntl.h>
 
 int main() {
+    int num1=10, num2=2, num3=7;
+    FILE* fptr = open("data.txt","w");
+    fprintf(fptr, "%d %%d %d", num1,num2,num3);
 
+    //sequential forking
 }
