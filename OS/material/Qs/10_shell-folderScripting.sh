cdb6a16256e911b054434ccc0dc1f25b53adc83e
Syed-56 <ssa.56.sultan@gmail.com>
2026-03-30 14:46:30 +0500

    update

--- OS/material/Qs/10_shell-folderScripting.sh
+++ OS/material/Qs/10_shell-folderScripting.sh
@@ -3,5 +3,23 @@
 while true
 do
 	echo -e  "\nOS Menu\n1) Create Folder\n2)List Folders\n3)Count Folders\n"
-	read -p "Enter Option: " opt
+        read -p "Enter Option: " opt
+        if [[ opt -gt 3 || opt -lt 0 ]]; then
+                echo -e "Invalid Option\n"
+                continue
+        fi
+	
+	if [ $opt -eq 1 ]; then
+		#create folder
+	fi
+
+	if [ $opt -eq 2 ]; then
+                #list folder
+        fi
+
+	if [ $opt -eq 3 ]; then
+                #count folder
+        fi
+
+
 done
